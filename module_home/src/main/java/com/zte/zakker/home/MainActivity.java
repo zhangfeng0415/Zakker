1c70098611e8e31560c0239e34cef3d239a49999
zhangfeng <2498711309@qq.com>
2021-08-22 23:51:36 +0800

    modify home page view

--- module_home/src/main/java/com/zte/zakker/home/MainActivity.java
+++ module_home/src/main/java/com/zte/zakker/home/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, MainHomeFragment.newInstance()).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, MainHomeFragment.newInstance()).commit();
     }
 }
